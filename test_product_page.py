c44a4b359e966892b43038f34b72edce40b68a62
ilya radovilsky <ilya.radovilsky@parovoz.tv>
2020-01-17 16:27:02 +0300

    added parametrized test of several product pages

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,18 @@
 # encoding: utf8
 
+import pytest
 from .pages.product_page import ProductPage
 import time
 
+link_template = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer%d'
+links_to_test = [link_template % i for i in range(10)]
+links_to_test[7] = pytest.param(links_to_test[7], marks=pytest.mark.xfail)
 
-def test_guest_can_add_product_to_basket(browser):
+
+@pytest.mark.parametrize('link', links_to_test)
+def test_guest_can_add_product_to_basket(browser, link):
     # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
